7e14e917e38013e0b9cfb86e104b16232ffb5234
pope04527-a11y <pope04527@gmail.com>
2026-01-20 00:36:11 +0800

    Update Notifications.jsx

--- src/pages/Notifications.jsx
+++ src/pages/Notifications.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const BACKEND_URL = "http://localhost:3002";
+const BACKEND_URL = "https://stacksapp-backend-main.onrender.com";
 const START_BLUE = "#1fb6fc";
 
 export default function Notifications() {
