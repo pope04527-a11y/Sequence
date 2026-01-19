9a128afbc40ce6df123fe3c0ffafa367bd907c83
pope04527-a11y <pope04527@gmail.com>
2026-01-20 04:11:54 +0800

    Update AppRoutes.jsx

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate, useParams } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate, useParams } from "react-router-dom";
 
 // Public pages
 import Login from "../pages/Login.jsx";
