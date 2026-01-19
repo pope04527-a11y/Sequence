029bb459469be562819772f1bb7831b52792ed30
pope04527-a11y <pope04527@gmail.com>
2026-01-20 00:37:25 +0800

    Update UpdateWithdrawPassword.jsx

--- src/pages/UpdateWithdrawPassword.jsx
+++ src/pages/UpdateWithdrawPassword.jsx
@@ -24,7 +24,7 @@ export default function UpdateWithdrawPassword() {
     setLoading(true);
     try {
       const token = localStorage.getItem("authToken");
-      const BASE_URL = "http://localhost:3002";
+      const BASE_URL = "https://stacksapp-backend-main.onrender.com";
       const res = await fetch(`${BASE_URL}/api/change-withdraw-password`, {
         method: "POST",
         headers: {
