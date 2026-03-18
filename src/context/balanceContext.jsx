668d7ce943b8019d6892d5ac5a9c4b97acfba23f
pope04527-a11y <pope04527@gmail.com>
2026-03-18 20:19:52 +0300

    Update balanceContext.jsx

--- src/context/balanceContext.jsx
+++ src/context/balanceContext.jsx
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
 
-const BASE_URL = "https://stacksapp-backend-main.onrender.com";
+const BASE_URL = "https://sequence-commerce-nsph.onrender.com";
 
 export const BalanceContext = createContext();
 
