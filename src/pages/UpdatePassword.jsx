8f76e3d5d7707dbcd9c5fba484de3c21bb4e8e67
pope04527-a11y <pope04527@gmail.com>
2026-03-18 20:25:08 +0300

    Update UpdatePassword.jsx

--- src/pages/UpdatePassword.jsx
+++ src/pages/UpdatePassword.jsx
@@ -76,7 +76,7 @@ export default function UpdatePassword() {
     setLoading(true);
     try {
       const token = localStorage.getItem("authToken");
-      const BASE_URL = "https://stacksapp-backend-main.onrender.com";
+      const BASE_URL = "https://sequence-commerce-nsph.onrender.com";
       const res = await fetch(`${BASE_URL}/api/change-password`, {
         method: "POST",
         headers: {
