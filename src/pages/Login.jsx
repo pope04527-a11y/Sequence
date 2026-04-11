a9d17dd7ce9c0a301fa7a34bc66b82107e05d918
pope04527-a11y <pope04527@gmail.com>
2026-04-11 19:51:01 +0300

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -100,7 +100,7 @@ function SpinnerOverlay({ duration = 500, onDone }) {
   );
 }
 
-const API_URL = "https://sequence-commerce-nsph.onrender.com";
+const API_URL = "https://sequence-backend-5v07.onrender.com";
 
 export default function Login({ refreshRecords }) {
   const [input, setInput] = useState("");
