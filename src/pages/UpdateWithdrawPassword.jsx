aaf418db88b54b950659aa4f370c8bc2f9a42e4c
pope04527-a11y <pope04527@gmail.com>
2026-04-27 15:33:59 +0300

    Update UpdateWithdrawPassword.jsx

--- src/pages/UpdateWithdrawPassword.jsx
+++ src/pages/UpdateWithdrawPassword.jsx
@@ -24,7 +24,7 @@ export default function UpdateWithdrawPassword() {
     setLoading(true);
     try {
       const token = localStorage.getItem("authToken");
-      const BASE_URL = "https://sequence-backend-5v07.onrender.com";
+      const BASE_URL = "https://sequence-admins.onrender.com";
       const res = await fetch(`${BASE_URL}/api/change-withdraw-password`, {
         method: "POST",
         headers: {
